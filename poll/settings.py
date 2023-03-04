9cc7ca200991bcfe8ebaa7723a3274ac4dfa0c85
Pggeeks <82941619+Pggeeks@users.noreply.github.com>
2023-03-04 13:44:34 +0530

    Update settings.py

--- poll/settings.py
+++ poll/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4+l497^$z(8&=z9du%@--z1#omkftj&z&5(2pqkn@g+(uq45dz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
